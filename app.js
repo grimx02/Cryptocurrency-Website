5dcf0ce0e72e1c1916733aa4aed0acb46ccea832
grimx02 <115450826+grimx02@users.noreply.github.com>
2025-04-09 10:27:35 +1200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// This code allows the menu (the three lines on the navbar) to close when a link is clicked
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
